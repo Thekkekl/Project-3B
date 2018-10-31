334e794ef4a2e3d6345d5276c08ce2cb41055e90
Thekkekl <42657228+Thekkekl@users.noreply.github.com>
2018-10-30 20:51:18 -0600

    Add files via upload

---
+++ Project 3B/Project 3B/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace Project_3B
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbtnFemale = new System.Windows.Forms.RadioButton();
+            this.rbtnMale = new System.Windows.Forms.RadioButton();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.txtInches = new System.Windows.Forms.TextBox();
+            this.lblBMI = new System.Windows.Forms.Label();
+            this.txtBody = new System.Windows.Forms.TextBox();
+            this.txtWeight = new System.Windows.Forms.TextBox();
+            this.txtFeet = new System.Windows.Forms.TextBox();
+            this.lblWeight = new System.Windows.Forms.Label();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.btnMainMenu = new System.Windows.Forms.Button();
+            this.BtnCalculate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(456, 26);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(97, 91);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 34;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(571, 233);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 20);
+            this.label3.TabIndex = 33;
+            this.label3.Text = "pounds";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(571, 160);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 20);
+            this.label2.TabIndex = 32;
+            this.label2.Text = "inches";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(378, 164);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 20);
+            this.label1.TabIndex = 31;
+            this.label1.Text = "feet";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbtnFemale);
+            this.groupBox1.Controls.Add(this.rbtnMale);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(162, 26);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(209, 108);
+            this.groupBox1.TabIndex = 30;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Gender:";
+            // 
+            // rbtnFemale
+            // 
+            this.rbtnFemale.AutoSize = true;
+            this.rbtnFemale.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtnFemale.Location = new System.Drawing.Point(25, 25);
+            this.rbtnFemale.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.rbtnFemale.Name = "rbtnFemale";
+            this.rbtnFemale.Size = new System.Drawing.Size(85, 24);
+            this.rbtnFemale.TabIndex = 1;
+            this.rbtnFemale.TabStop = true;
+            this.rbtnFemale.Text = "&Female";
+            this.rbtnFemale.UseVisualStyleBackColor = true;
+            // 
+            // rbtnMale
+            // 
+            this.rbtnMale.AutoSize = true;
+            this.rbtnMale.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtnMale.Location = new System.Drawing.Point(25, 65);
+            this.rbtnMale.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.rbtnMale.Name = "rbtnMale";
+            this.rbtnMale.Size = new System.Drawing.Size(66, 24);
+            this.rbtnMale.TabIndex = 2;
+            this.rbtnMale.TabStop = true;
+            this.rbtnMale.Text = "&Male";
+            this.rbtnMale.UseVisualStyleBackColor = true;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResult.Location = new System.Drawing.Point(334, 353);
+            this.txtResult.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(195, 26);
+            this.txtResult.TabIndex = 29;
+            this.txtResult.TabStop = false;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(158, 361);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(62, 20);
+            this.lblResult.TabIndex = 28;
+            this.lblResult.Text = "Result:";
+            // 
+            // txtInches
+            // 
+            this.txtInches.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInches.Location = new System.Drawing.Point(456, 155);
+            this.txtInches.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtInches.Name = "txtInches";
+            this.txtInches.Size = new System.Drawing.Size(96, 26);
+            this.txtInches.TabIndex = 21;
+            // 
+            // lblBMI
+            // 
+            this.lblBMI.AutoSize = true;
+            this.lblBMI.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBMI.Location = new System.Drawing.Point(158, 294);
+            this.lblBMI.Name = "lblBMI";
+            this.lblBMI.Size = new System.Drawing.Size(142, 20);
+            this.lblBMI.TabIndex = 26;
+            this.lblBMI.Text = "Body Mass Index:";
+            // 
+            // txtBody
+            // 
+            this.txtBody.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBody.Location = new System.Drawing.Point(334, 287);
+            this.txtBody.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtBody.Name = "txtBody";
+            this.txtBody.ReadOnly = true;
+            this.txtBody.Size = new System.Drawing.Size(195, 26);
+            this.txtBody.TabIndex = 27;
+            this.txtBody.TabStop = false;
+            // 
+            // txtWeight
+            // 
+            this.txtWeight.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtWeight.Location = new System.Drawing.Point(334, 220);
+            this.txtWeight.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtWeight.Name = "txtWeight";
+            this.txtWeight.Size = new System.Drawing.Size(195, 26);
+            this.txtWeight.TabIndex = 23;
+            // 
+            // txtFeet
+            // 
+            this.txtFeet.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFeet.Location = new System.Drawing.Point(274, 155);
+            this.txtFeet.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtFeet.Name = "txtFeet";
+            this.txtFeet.Size = new System.Drawing.Size(96, 26);
+            this.txtFeet.TabIndex = 20;
+            // 
+            // lblWeight
+            // 
+            this.lblWeight.AutoSize = true;
+            this.lblWeight.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWeight.Location = new System.Drawing.Point(158, 224);
+            this.lblWeight.Name = "lblWeight";
+            this.lblWeight.Size = new System.Drawing.Size(66, 20);
+            this.lblWeight.TabIndex = 22;
+            this.lblWeight.Text = "&Weight:";
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeight.Location = new System.Drawing.Point(158, 159);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(63, 20);
+            this.lblHeight.TabIndex = 19;
+            this.lblHeight.Text = "&Height:";
+            // 
+            // btnMainMenu
+            // 
+            this.btnMainMenu.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMainMenu.Location = new System.Drawing.Point(435, 417);
+            this.btnMainMenu.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnMainMenu.Name = "btnMainMenu";
+            this.btnMainMenu.Size = new System.Drawing.Size(148, 58);
+            this.btnMainMenu.TabIndex = 25;
+            this.btnMainMenu.Text = "E&xit to Main Menu";
+            this.btnMainMenu.UseVisualStyleBackColor = true;
+            this.btnMainMenu.Click += new System.EventHandler(this.btnMainMenu_Click);
+            // 
+            // BtnCalculate
+            // 
+            this.BtnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnCalculate.Location = new System.Drawing.Point(200, 417);
+            this.BtnCalculate.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.BtnCalculate.Name = "BtnCalculate";
+            this.BtnCalculate.Size = new System.Drawing.Size(148, 58);
+            this.BtnCalculate.TabIndex = 24;
+            this.BtnCalculate.Text = "&Calculate";
+            this.BtnCalculate.UseVisualStyleBackColor = true;
+            this.BtnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(771, 503);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.txtInches);
+            this.Controls.Add(this.lblBMI);
+            this.Controls.Add(this.txtBody);
+            this.Controls.Add(this.txtWeight);
+            this.Controls.Add(this.txtFeet);
+            this.Controls.Add(this.lblWeight);
+            this.Controls.Add(this.lblHeight);
+            this.Controls.Add(this.btnMainMenu);
+            this.Controls.Add(this.BtnCalculate);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbtnFemale;
+        private System.Windows.Forms.RadioButton rbtnMale;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox txtInches;
+        private System.Windows.Forms.Label lblBMI;
+        private System.Windows.Forms.TextBox txtBody;
+        private System.Windows.Forms.TextBox txtWeight;
+        private System.Windows.Forms.TextBox txtFeet;
+        private System.Windows.Forms.Label lblWeight;
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.Button btnMainMenu;
+        private System.Windows.Forms.Button BtnCalculate;
+    }
+}
+
---
+++ Project 3B/Project 3B/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_3B
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+            string x = txtWeight.Text;
+            for (int i = 0; i <= x.Length; i++)
+            {
+                x = x.Replace("$", "");
+                x = x.Replace("@", "");
+                x = x.Replace("#", "");
+                x = x.Replace("%", "");
+                x = x.Replace("&", "");
+                x = x.Replace("^", "");
+                x = x.Replace("*", "");
+                x = x.Replace("[", "");
+                x = x.Replace("]", "");
+                x = x.Replace("/", "");
+
+            }
+            txtWeight.Text = x;
+
+            string y = txtFeet.Text;
+            if (y.StartsWith(" ") || y.StartsWith("$") || y.StartsWith("@") || y.StartsWith("%") || y.StartsWith("#"))
+            {
+                MessageBox.Show("The string2 can't start with a space or $ or @ or % or #");
+                txtFeet.Clear();
+                txtFeet.Focus();
+            }
+            string z = txtInches.Text;
+            if (z.EndsWith("0") || z.EndsWith("1") || z.EndsWith("2") || z.EndsWith("3"))
+            {
+                MessageBox.Show("The string3 can't End with 0, 1, 2, 3");
+                txtInches.Clear();
+                txtInches.Focus();
+                double weight = Convert.ToDouble(txtWeight.Text);
+                const double convertkg = 0.45;
+                double weightkg = weight * convertkg;
+
+                int feet = Convert.ToInt32(txtFeet.Text);
+                int inch = Convert.ToInt32(txtInches.Text);
+                int heightinch = (feet * 12) + inch;
+                const double convertmtr = 0.025;
+                double heightmtr = heightinch * convertmtr;
+                double heightsqr = Math.Pow(heightmtr, 2);
+
+                double bmi = weightkg / heightsqr;
+                string textResult = " ";
+
+
+                if (rbtnFemale.Checked)
+                {
+                    if (bmi < 18)
+                    {
+                        textResult = "Underweight";
+                    }
+                    else if (bmi >= 18 && bmi < 25)
+                    {
+                        textResult = "Normal";
+                    }
+                    else if (bmi >= 25 && bmi < 30)
+                    {
+                        textResult = "Overweight";
+                    }
+                    else if (bmi >= 30)
+                    {
+                        textResult = "Obese";
+                    }
+
+                }
+
+                else if (rbtnMale.Checked)
+                {
+                    if (bmi < 19)
+                    {
+                        textResult = "Underweight";
+                    }
+                    else if (bmi >= 19 && bmi < 26)
+                    {
+                        textResult = "Normal";
+
+                    }
+                    else if (bmi >= 26 && bmi < 31)
+                    {
+                        textResult = "Overweight";
+                    }
+                    else if (bmi >= 31)
+                    {
+                        textResult = "Obese";
+                    }
+                }
+                txtResult.Text = textResult;
+                txtBody.Text = bmi.ToString("n2");
+
+
+            }
+        }
+
+        private void btnMainMenu_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
